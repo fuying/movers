c4894acad34557e7071e5defca35b5337b87dac5
fuying <xianfuying@126.com>
2012-11-12 17:28:35 +0800

    init repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Movers::Application.config.secret_token = '5885fc3e5a9b8f888445469f9e251cca4a25e867b91463d3e164cb5f08da09323d856904e8f11eea2785d250741f2c7d60cb0dc836cbf07a94299d3a80d4ec31'
